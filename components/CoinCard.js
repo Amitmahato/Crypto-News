8c5756e86fc4c6d750ceca80024a229ded2dafd0
amitmahato <073bct505.amit@pcampus.edu.np>
2019-11-16 10:33:52 +0545

    designed card for cryptocurrency coin

---
+++ components/CoinCard.js
@@ -0,0 +1,119 @@
+import React from "react";
+import { Dimensions } from "react-native";
+import styled from "styled-components";
+
+const screenWidth = Dimensions.get("window").width;
+const CoinCard = props => {
+  return (
+    <Container>
+      <Logo source={{ uri: props.logo }} />
+      <Content>
+        <Header>
+          <Title>
+            <Enum>{props.symbol}</Enum>
+            <Name>{props.name}</Name>
+          </Title>
+          <Price>{Number(props.price_usd).toFixed(2)}$</Price>
+        </Header>
+        <Footer>
+          <Daily>
+            <Time>24h : </Time>
+            <Change24hr
+              style={props.percent_change_24h < 0 ? { color: "#f55" } : {}}
+            >
+              {props.percent_change_24h}%
+            </Change24hr>
+          </Daily>
+          <Weekly>
+            <Time>7d : </Time>
+            <Change7d
+              style={props.percent_change_7d < 0 ? { color: "#f55" } : {}}
+            >
+              {props.percent_change_7d}%
+            </Change7d>
+          </Weekly>
+        </Footer>
+      </Content>
+    </Container>
+  );
+};
+
+export default CoinCard;
+
+const Container = styled.View`
+  width: 100%;
+  height: 100px;
+  padding: 10px;
+  margin-top: 25px;
+  background: white;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  border-bottom-width: 2px;
+  border-bottom-color: gray;
+`;
+
+const Logo = styled.Image`
+  width: 55px;
+  height: 55px;
+`;
+
+const Content = styled.View`
+  width: ${screenWidth - 100};
+`;
+
+const Header = styled.View`
+  width: 100%;
+  height: 50px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom-width: 2px;
+  border-bottom-color: rgba(80, 80, 80, 0.1);
+`;
+
+const Title = styled.View`
+  flex-direction: row;
+`;
+
+const Enum = styled.Text`
+  text-transform: uppercase;
+  font-size: 20px;
+  border-right-width: 2px;
+  padding-right: 5px;
+`;
+const Name = styled.Text`
+  font-size: 20px;
+  padding-left: 5px;
+  color: rgba(0, 0, 0, 0.7);
+`;
+const Price = styled.Text`
+  color: black;
+  font-size: 18px;
+`;
+
+const Footer = styled.View`
+  margin-top: 10px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Daily = styled.View`
+  flex-direction: row;
+`;
+const Weekly = styled.View`
+  flex-direction: row;
+`;
+const Time = styled.Text`
+  font-size: 16px;
+`;
+
+const Change24hr = styled.Text`
+  font-size: 16px;
+  color: #33cccc;
+`;
+const Change7d = styled.Text`
+  font-size: 16px;
+  color: #33cccc;
+`;
